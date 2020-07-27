6a748a544fe9101f544502bd88984faeb7cb3fcc
guibin-zhang <z_guibin@163.com>
2020-07-27 08:12:57 +0800

    添加文件

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,12 @@
+/**
+ * @author gb.z
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/7/27 8:07
+ * @Copyright © 2020 新致（北京）有限公司
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("可以了");
+    }
+}
